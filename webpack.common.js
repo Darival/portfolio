2ac312a476546862c3d5a23fabfbe7123720f5e5
Jorge Gil <jorgep13@piso13.com>
2020-01-19 01:21:25 -0600

    add dev and prod webpack config files

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,7 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  //devtool: "none",
-  output: {
-    filename: "main-[contentHash].js",
-    path: path.resolve(__dirname, "dist")
-  },
   module: {
     rules: [
       {
