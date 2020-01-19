0145f6934e323b26ac1cd8d0425bf1ecfa3e0623
Jorge Gil <jorgep13@piso13.com>
2020-01-18 23:13:48 -0600

    add webpack scss loaders

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { run } from "./app/app.js";
 import { NavService } from "./app/utils/nav-service";
 import { HeaderService } from "./app/utils/header-service.js";
 
+import "./style/style.scss";
+
 const navService = new NavService();
 const headerService = new HeaderService();
 
